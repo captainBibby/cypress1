72db9ed99c658054ee5099c49b9949304e09bf07
David Bibić <davidbibic@icloud.com>
2022-12-03 16:46:33 +0100

    updated negative test cases

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -2,33 +2,97 @@
 
 const locators = require("../fixtures/locators.json");
 
+function makeId(length) {
+  var result = "";
+  var characters =
+    "abcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    );
+  }
+  return result;
+}
+
+let email = `${makeId(5)}@test.com`;
+
 describe("registration test", () => {
 
-    function makeId(length) {
-        var result = "";
-        var characters =
-          "abcdefghijklmnopqrstuvwxyz0123456789";
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-          );
-        }
-        
-        return result;
-      }
-      let email = `${makeId(5)}@test.com`;
+  beforeEach ("visit gallery app", () => {
+    cy.visit("/");
+    cy.get(locators.Register.registerButton).click();
+  });
 
-    it("register with valid data", () => {
-        cy.visit("/");
-        cy.get(locators.Register.registerButton).click();
-        cy.get(locators.Register.firstNameInput).type("David");
-        cy.get(locators.Register.lastNameInput).type("Bibi");
-        cy.get(locators.Common.emailInput).type(email);
-        cy.get(locators.Common.passwordInput).type("1111111a");
-        cy.get(locators.Register.passwordConfirmInput).type("1111111a");
-        cy.get(locators.Register.checkbox).check();
-        cy.get(locators.Common.submitButton).click();
-        cy.url().should("not.include", "/register");
-    })
+  it ("register without email", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibic");
+    cy.get(locators.Common.passwordInput).type("1234567a");
+    cy.get(locators.Register.passwordConfirmInput).type("1234567a");
+    cy.get(locators.Register.checkbox).check();
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
+
+  it ("register with invalid email format", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibic");
+    cy.get(locators.Common.emailInput).type("blabla");
+    cy.get(locators.Common.passwordInput).type("1234567a");
+    cy.get(locators.Register.passwordConfirmInput).type("1234567a");
+    cy.get(locators.Register.checkbox).check();
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
+
+  it ("register without password", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibic");
+    cy.get(locators.Common.emailInput).type(email);
+    cy.get(locators.Common.passwordInput).type("1234567a");
+    cy.get(locators.Register.checkbox).check();
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
+
+  it ("register without password confirmation", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibic");
+    cy.get(locators.Common.emailInput).type(email);
+    cy.get(locators.Common.passwordInput).type("1234567a");
+    cy.get(locators.Register.checkbox).check();
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
+
+  it ("register with sufficient password characters", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibic");
+    cy.get(locators.Common.emailInput).type(email);
+    cy.get(locators.Common.passwordInput).type("123aaa");
+    cy.get(locators.Common.passwordInput).type("123aaa");
+    cy.get(locators.Register.checkbox).check();
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
+
+  it ("register without checking checkbox", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibic");
+    cy.get(locators.Common.emailInput).type(email);
+    cy.get(locators.Common.passwordInput).type("123aaa");
+    cy.get(locators.Common.passwordInput).type("123aaa");
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
+
+  it("register with valid data", () => {
+    cy.get(locators.Register.firstNameInput).type("David");
+    cy.get(locators.Register.lastNameInput).type("Bibi");
+    cy.get(locators.Common.emailInput).type(email);
+    cy.get(locators.Common.passwordInput).type("1111111a");      cy.get(locators.Register.passwordConfirmInput).type("1111111a");
+    cy.get(locators.Register.checkbox).check();
+    cy.get(locators.Common.submitButton).click();
+    cy.url().should("not.include", "/register");
+  });
 });
