6be4a8985c9f14dc89a6cf75437189e2dfddb9aa
David Bibić <davidbibic@icloud.com>
2022-12-05 20:50:29 +0100

    new locators arrangement

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,14 +5,14 @@ const locators = require("../fixtures/locators.json");
 describe ("login test", () => {
     it ("login with valid credentials", () => {
         cy.visit("/");
-        cy.get(locators.Login.loginButton).click();
+        cy.get(locators.Header.loginButton).click();
         cy.get(locators.Common.emailInput).type("bibi@gmail.com");
         cy.get(locators.Common.passwordInput).type("1111111a");
         cy.get(locators.Common.submitButton).click();
     });
 
     it ("logout", () => {
-        cy.get(locators.Logout.logoutButton).should("have.length", 4); //ceka dok ne ucita 4 elementa
-        cy.get(locators.Logout.logoutButton).eq(3).click();
+        cy.get(locators.Header.logoutButton).should("have.length", 4); //ceka dok ne ucita 4 elementa
+        cy.get(locators.Header.logoutButton).eq(3).click();
     });
 });
