07ae579fc0d7227dbfe606ca4d41dc6c6688cfc4
David Bibić <davidbibic@icloud.com>
2022-12-06 20:13:06 +0100

    updated login tests with negative cases using faker

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,44 @@
 /// <reference types="Cypress"/>
 
+import { faker } from '@faker-js/faker';
+
 const locators = require("../fixtures/locators.json");
 
+let invalidData = {
+    invalidEmail: faker.internet.email(),
+    invalidPassword: faker.internet.password()
+  };
+
 describe ("login test", () => {
-    it ("login with valid credentials", () => {
+
+    beforeEach ("visit login page", () => {
         cy.visit("/");
         cy.get(locators.Header.loginButton).click();
-        cy.get(locators.Common.emailInput).type("bibi@gmail.com");
+    });
+
+    it ("negative case / login with invalid email", () => {
+        cy.get(locators.Common.emailInput).type(invalidData.invalidEmail);
         cy.get(locators.Common.passwordInput).type("1111111a");
         cy.get(locators.Common.submitButton).click();
     });
 
-    it ("logout", () => {
+    it ("negative case / login with invalid password", () => {
+        cy.get(locators.Common.emailInput).type("bibi@gmail.com");
+        cy.get(locators.Common.passwordInput).type(invalidData.invalidPassword);
+        cy.get(locators.Common.submitButton).click();
+    })
+
+    it ("negative case / login with invalid credentials", () => {
+        cy.get(locators.Common.emailInput).type(invalidData.invalidEmail);
+        cy.get(locators.Common.passwordInput).type(invalidData.invalidPassword);
+        cy.get(locators.Common.submitButton).click();
+    })
+
+    it ("login with valid credentials", () => {
+        cy.get(locators.Common.emailInput).type("bibi@gmail.com");
+        cy.get(locators.Common.passwordInput).type("1111111a");
+        cy.get(locators.Common.submitButton).click();
         cy.get(locators.Header.logoutButton).should("have.length", 4); //ceka dok ne ucita 4 elementa
         cy.get(locators.Header.logoutButton).eq(3).click(); 
     });
 });
-
-//git pravi problem
